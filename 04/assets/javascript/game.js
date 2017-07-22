f41a5cde93113ad6113712ae1934fdf14b620a20
Brandon Fulljames <bfullj@gmail.com>
2017-07-21 20:59:24 -0700

    Initialize game function

--- 04/assets/javascript/game.js
+++ 04/assets/javascript/game.js
@@ -1,1 +1,16 @@
-console.log('hi');
+var targetScore;
+var userScore;
+var totalWins;
+var totalLosses;
+var crystalScores = [];
+
+function startGame() {
+  targetScore = Math.floor(Math.random() * 101 + 19);
+  userScore = 0;
+
+  for (var i = 0; i < 4; i++) {
+    crystalScores[i] = Math.floor(Math.random() * 12 + 1);
+  }
+}
+
+startGame();
